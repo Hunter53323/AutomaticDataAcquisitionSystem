5ac5f6c0a31f7caffe57e86ae4ad1161cf21b8b2
liuyuxiang <953973860@qq.com>
2024-07-12 21:29:08 +0800

    debug

--- core/auto_collection/__init__.py
+++ core/auto_collection/__init__.py
@@ -1,4 +1,5 @@
 from .auto_collection import AutoCollection
 from ..communication import communicator
+from ..database import outputdb
 
-auto_collector = AutoCollection(communicator)
+auto_collector = AutoCollection(communicator, outputdb)
