c7c41b7e9580831d0b66bcdd9cce7f1658405c88
liuyuxiang <953973860@qq.com>
2024-07-18 21:57:20 +0800

    更新main

--- main.py
+++ main.py
@@ -1,120 +1,44 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask_cors import CORS
-# from core.communication import communicator
-# from core.auto_collection import auto_collector
-import threading
-
-############新增数据库模块导入
-from flask import Flask, request, jsonify, render_template
-from core.database import outputdb
-import math
-############################3
-
+from application.auto_collection import autocollect
+from application.database import db
+from application.device_control import control
+from application.websocket.real_time_dataapi import init_socketio_events
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
 socketio = SocketIO(app)
+CORS(app, supports_credentials=True)
 
-# communicator.connect()
-# communicator.start_read_all()
-
-thread = None
-thread_running = threading.Event()
+app.register_blueprint(autocollect, url_prefix="/collect")
+app.register_blueprint(db, url_prefix="/db")
+app.register_blueprint(control, url_prefix="/control")
+init_socketio_events(app)
 
 
 @app.route("/")
 def index():
+    # 旧版页面
     return render_template("index.html")
 
+
 # 路由到第二个index.html
-@app.route('/second')
+@app.route("/second")
 def index_database():
-    return render_template('index_database.html')
+    return render_template("index_database.html")
 
 
-# 初始化数据库连接
-
-outputdb.create_table()  # 创建表结构
-
-
-@app.route('/api/data', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def api_data():
-    if request.method == 'POST':
-        # 处理数据插入
-        data_list = request.get_json().get('data_list', [])
-        outputdb.insert_data(data_list)
-        return jsonify({'status': 'success', 'message': 'Data inserted successfully'})
-    elif request.method == 'GET':
-        # 默认为GET请求，返回数据列表
-        ids_input = request.args.get('ids_input', None)
-        columns = request.args.getlist('columns', None)
-        conditions = request.args.get('conditions', None)
-        if ids_input:
-            # 分割字符串，并转换为整数列表
-            ids = [int(id_str) for id_str in ids_input.split(',')]
-        else:
-            ids = None
-        selected_data = outputdb.select_data(ids_input=ids, columns=columns, conditions=conditions)
-        return jsonify(selected_data)
-    elif request.method == 'PUT':
-        # 处理数据更新
-        data = request.get_json()
-        ids = data.get('ids')
-        update_data = data.get('update_data')
-        # 将ids字符串转换为整数列表
-        ids = [int(id_str) for id_str in ids.split(',') if id_str.isdigit()]
-        outputdb.update_data(ids, update_data)
-        return jsonify({'status': 'success', 'message': 'Data updated successfully'})
-    elif request.method == 'DELETE':
-        # 处理数据删除
-        ids_input = request.get_json().get('ids_input')
-        outputdb.delete_data_by_ids(ids_input)
-        return jsonify({'status': 'success', 'message': 'Data deleted successfully'})
-
-@app.route('/api/export', methods=['GET'])
-def api_export():
-    # filename = 'fans_data1.csv'
-    filename = request.args.get('filename')
-    ids_input = request.args.get('ids_input', None)
-    print(ids_input)
-    additional_conditions = request.args.get('additional_conditions', '')
-    print(additional_conditions)
-    try:
-        outputdb.export_data_with_conditions_to_csv(ids_input=ids_input, additional_conditions=additional_conditions, filename=filename)
-        return jsonify({'status': 'success', 'message': f'Data exported to {filename}'})
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)})
-
-
-@app.route('/api/data/page', methods=['GET'])
-def api_showall():
-    page = int(request.args.get('page', 1))
-    per_page = int(request.args.get('per_page', 10))
-    offset = (page - 1) * per_page
-    cursor = outputdb.connection.cursor()
-    try:
-        cursor.execute("SELECT COUNT(*) FROM 风机数据")
-        total_count = cursor.fetchone()[0]
-        cursor.execute("SELECT * FROM 风机数据 LIMIT %s OFFSET %s", (per_page, offset))
-        data = cursor.fetchall()
-        total_pages = (total_count + per_page - 1) // per_page  # 计算总页数
-        return jsonify({
-            'data': data,
-            'page': page,
-            'per_page': per_page,
-            'total_count': total_count,  # 确保返回总记录数
-            'total_pages': total_pages  # 返回计算后的总页数
-        })
-    finally:
-        cursor.close()
-
-@app.route('/api/clear_data', methods=['DELETE'])
-def api_clear_data():
-    # 调用数据库的删除函数，例如删除所有ID的数据
-    outputdb.delete_data_by_ids(None)  # None 表示删除所有数据
-    return jsonify({'status': 'success', 'message': 'Database cleared successfully'})
+def get_apis():
+    # 获取当前所有api
+    print(app.url_map)
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="127.0.0.1", port=5000)
+    socketio.run(app, debug=True, host="127.0.0.1", port=5000, allow_unsafe_werkzeug=True)
+
+
+# 考虑浏览器页面意外关闭，也就是disconnect的时候，如何处理，如果有正在进行的自动采集，那么需要暂停自动采集
+# TODO：1.数据库导出的时候选择文件目录与文件名
+# TODO：2.数据库的ID应该怎么去弄
+# TODO：3.数采稳定判别逻辑最好移植到单独的函数里面去，写后清空当前数据后的判断考虑是否有更完善的方案
+# TODO：4.可以完成自动采集以及自动存储数据，加上一个从当前数据直接存储的功能
